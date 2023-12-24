ceefad5e64e75ddf013acf4076422f343c0c3e4f
Pooja Bhattacharya <125653047+poojabhattacharya10@users.noreply.github.com>
2023-12-24 19:37:23 +0530

    Add files via upload

---
+++ ReverseWords.java
@@ -0,0 +1,16 @@
+package STRING;
+
+public class ReverseWords {
+    static String reverse(String str){
+        String words[] = str.split("\\s+");
+        StringBuilder revStr = new StringBuilder();
+        for(int i = words.length-1 ; i >= 0 ; i--){
+            revStr.append(words[i]);
+            revStr.append(" ");
+        }
+        return revStr.toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(reverse("the sky is blue"));
+    }
+}
